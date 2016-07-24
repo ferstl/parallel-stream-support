f5aeb949f781cb1f10e99916822d977b946ad3c7
Stefan Ferstl <st.ferstl@gmail.com>
2016-07-24 13:59:28 +0200

    Add tets for thread name

--- src/test/java/com/github/ferstl/ParallelStreamSupportTest.java
+++ src/test/java/com/github/ferstl/ParallelStreamSupportTest.java
@@ -1,14 +1,24 @@
 package com.github.ferstl;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.junit.Assert.assertThat;
 
 
 public class ParallelStreamSupportTest {
+
+  private static final Pattern FORK_JOIN_THREAD_NAME_PATTERN = Pattern.compile("ForkJoinPool-\\d+-worker-\\d+");
 
   private ForkJoinPool workerPool;
 
@@ -23,11 +33,25 @@ public class ParallelStreamSupportTest {
     this.workerPool.awaitTermination(1, TimeUnit.SECONDS);
   }
 
+  /**
+   * Verify that we use the correct thread name pattern.
+   */
+  @Test
+  public void testForkJoinPoolThreadNamePattern() {
+    ForkJoinTask<String> task = ForkJoinTask.adapt(() -> Thread.currentThread().getName());
+    String threadName = this.workerPool.invoke(task);
+
+    assertThat(threadName, matchesPattern(FORK_JOIN_THREAD_NAME_PATTERN));
+  }
 
   @Test
-  public void test() {
-    ParallelStreamSupport.parallelStream(Arrays.asList("a"), this.workerPool)
-        .forEach(e -> System.out.println(Thread.currentThread().getName()));
+  @Ignore("Not yet implemented")
+  public void collect() {
+    List<String> result = ParallelStreamSupport.parallelStream(singletonList("a"), this.workerPool)
+        .map(e -> Thread.currentThread().getName())
+        .collect(toList());
+
+    assertThat(result, contains(matchesPattern(FORK_JOIN_THREAD_NAME_PATTERN)));
   }
 
 }
